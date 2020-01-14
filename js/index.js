f619da07506ad512f92a8f992c8724293efb3c37
unknown <eastkim1@hotmail.com>
2020-01-14 15:03:06 -0700

    Starting

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+const busimg = document.querySelector ('.intro img');
+busimg.addEventListener('mouseenter', e => {
+    busimg.setAttribute('src', 'img/fun-bus-reverse.jpg');
+});
+busimg.addEventListener('mouseleave', e => {
+    busimg.setAttribute('src', 'img/fun-bus.jpg');
+});
